293775040b2ef40451c48e9ef46614d6ad0a6f37
Sin Han <vrdhan212@gmail.com>
2024-07-12 13:26:18 +0900

    [ug-todo] update: create todo using redux

--- todo-upgrade/src/components/todo/input/TodoInput.tsx
+++ todo-upgrade/src/components/todo/input/TodoInput.tsx
@@ -1,21 +1,53 @@
-import * as React from "react";
+import { ThemeContext } from "@/providers/ThemeProvider";
+import THEME from "@/styles/theme";
+import { useContext } from "react";
 import styled from "styled-components";
-
-interface TodoInputProps {}
 
 const TodoInputBox = styled.div`
   display: flex;
-  flex-direction: column;
   align-items: center;
   justify-content: center;
+  margin-bottom: 5px;
 `;
 
-const TodoInput: React.FC<TodoInputProps> = () => {
+const TodoInputName = styled.span<{ theme: string }>`
+  color: ${({ theme }) =>
+    theme === "LIGHT"
+      ? THEME.COLOR.DARK.BACKGROUND
+      : THEME.COLOR.LIGHT.BACKGROUND};
+  margin-right: 5px;
+`;
+
+const Input = styled.input<{ theme: string }>`
+  width: 200px;
+  height: 30px;
+  border: none;
+  background-color: ${({ theme }) =>
+    theme === "LIGHT"
+      ? THEME.COLOR.LIGHT.BACKGROUND
+      : THEME.COLOR.DARK.BACKGROUND};
+`;
+
+interface TodoInputProps {
+  title: string;
+  placeholder: string;
+  value: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const TodoInput = ({ title, placeholder, value, onChange }: TodoInputProps) => {
+  const { theme } = useContext(ThemeContext);
   return (
     <>
       <TodoInputBox>
-        <span>이름</span>
-        <input type="text" placeholder="할 일을 입력하세요"></input>
+        <TodoInputName theme={theme}>{title}</TodoInputName>
+        <Input
+          theme={theme}
+          type="text"
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+        ></Input>
       </TodoInputBox>
     </>
   );
--- todo-upgrade/src/components/todo/input/TodoInputContainer.tsx
+++ todo-upgrade/src/components/todo/input/TodoInputContainer.tsx
@@ -1,9 +1,12 @@
-import React from "react";
 import styled from "styled-components";
+import {  useDispatch, useSelector } from "react-redux";
 import TodoInput from "./TodoInput";
 import TodoRegisterBtn from "./TodoRegisterBtn";
+import { useState } from "react";
+import { RootState } from "@/store";
+import { addTodo } from "@/store/action";
 
-const InputSection = styled.section`
+const InputSection = styled.form`
   display: flex;
   flex: 1, 1;
   justify-content: space-evenly;
@@ -17,13 +20,43 @@ const TodoInputContent = styled.div`
   justify-content: center;
 `;
 
-const TodoInputContainer: React.FC = () => {
+const TodoInputContainer = () => {
+  const [title, setTitle] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const todos = useSelector((state: RootState) => state.todos);
+  const dispatch = useDispatch();
+  
+  const handleTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  }
+
+  const handleDescription = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDescription(e.target.value);
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if(title === "" || description === ""){
+      alert("제목과 내용을 입력해주세요");
+      return;
+    }
+    dispatch(addTodo({
+      id: String(todos.length + 1),
+      title: title,
+      content: description,
+      isDone: false,
+      createdAt: new Date().toLocaleDateString(),
+    }))
+    setTitle("");
+    setDescription("");
+  }
+
   return (
     <>
-      <InputSection>
+      <InputSection onSubmit={handleSubmit}>
         <TodoInputContent>
-          <TodoInput></TodoInput>
-          <TodoInput></TodoInput>
+          <TodoInput title={"제목"} placeholder="오늘은 무엇을 할건가요?" value={title} onChange={handleTitle}></TodoInput>
+          <TodoInput title={"내용"} placeholder="할 일을 구체적으로 적어주세요" value={description} onChange={handleDescription}></TodoInput>
         </TodoInputContent>
         <TodoRegisterBtn></TodoRegisterBtn>
       </InputSection>
