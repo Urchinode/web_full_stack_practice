810d3db068726e1462f77e067f4f90a7a8c938db
Sin Han <vrdhan212@gmail.com>
2024-07-13 15:16:11 +0900

    [ug-todo] fix: fail web content in jest env
    dom, local storage

--- todo-upgrade/jest.config.ts
+++ todo-upgrade/jest.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "jest";
 
 const config: Config = {
   preset: "ts-jest", // Babel이 아닌 Typescript 환경에서 테스트
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
   verbose: true,
   testMatch: ["**/tests/**/*.test.ts?(x)"],
   // @는 src를 가리킴
@@ -10,11 +10,8 @@ const config: Config = {
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
-
-  globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.json",
-    },
+  transform: {
+    "^.+\\.ts$": "ts-jest",
   },
 };
 
