b28e6730727944c6b58a1ce59fc4fc6375a78534
Sin Han <vrdhan212@gmail.com>
2024-07-14 12:54:43 +0900

    [ug-todo] feat: font style

--- todo-upgrade/src/styles/global.ts
+++ todo-upgrade/src/styles/global.ts
@@ -1,7 +1,18 @@
 import { createGlobalStyle } from "styled-components";
 import THEME from "./theme";
+import GoormSans from "@/assets/fonts/goorm-sans-regular.woff2";
 
 const GlobalStyle = createGlobalStyle`
+    @font-face {
+        font-family: 'goorm-sans';
+        src: url(${GoormSans}) format('woff2');
+        /* font-family: 'PartialSansKR'; */
+        /* src: url('../assets/fonts/PartialSansKR-Regular.otf') format('opentype'); */
+    }
+    /* @font-face { */
+        /* font-family: 'otfFont'; */
+        /* src: url() format('opentype'); */
+    /* } */
     .light{
         background-color: ${THEME.COLOR.LIGHT.BACKGROUND};
     }
@@ -11,7 +22,7 @@ const GlobalStyle = createGlobalStyle`
     html{
         height: 100%;
         width: 100%;
-        font-family: Inter, system-ui;
+        font-family: 'goorm-sans';
     }
     body{
         height: 100%;
