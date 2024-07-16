5fc9edefe3b7904c185de1c9590782f6dcf9d4b7
Sin Han <vrdhan212@gmail.com>
2024-07-16 16:30:22 +0900

    [dp] refactor: insert func

--- dp/src/main/java/com/server/Main.java
+++ dp/src/main/java/com/server/Main.java
@@ -4,10 +4,12 @@ import com.server.dao.ConnectionSingleton;
 import com.server.dao.QueryExecutor;
 import com.server.dao.TableEnum;
 import com.server.entity.User;
+import com.server.mapper.UserMapper;
 import com.server.utils.Logger;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -16,8 +18,13 @@ public class Main {
         org.slf4j.Logger logger = Logger.getLogger(Main.class);
         QueryExecutor qe = new QueryExecutor();
         try {
-            qe.insert(TableEnum.USER, User.getFields(), new User(4, "name", 30).getValues());
-            logger.info("INSERT COMPLETED");
+//            qe.insert(TableEnum.USER, User.getFields(), new User(4, "name", 30).getValues());
+//            logger.info("INSERT COMPLETED");
+            ArrayList<User> users = (ArrayList<User>) qe.selectAll(TableEnum.USER, new UserMapper());
+            for (User user : users) {
+                logger.info(String.valueOf(user));
+            }
+
         } catch (SQLException e) {
             logger.error("SQL EXECUTION FAILED");
             logger.error(e.getMessage());
--- dp/src/main/java/com/server/dao/QueryExecutor.java
+++ dp/src/main/java/com/server/dao/QueryExecutor.java
@@ -1,10 +1,14 @@
 package com.server.dao;
 
+import com.server.mapper.MapperInterface;
+import com.server.mapper.UserMapper;
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.MessageFormat;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -12,18 +16,21 @@ public class QueryExecutor {
     private final static Connection conn = ConnectionSingleton.getConnection();
 
     private static String toSQLString(Object origin) {
-        if(origin instanceof String) return "'" + origin + "'";
+        if (origin instanceof String) return "'" + origin + "'";
         else return String.valueOf(origin);
     }
 
-    public void selectAll(TableEnum tableName) throws SQLException {
+    public ArrayList<?> selectAll(TableEnum tableName, MapperInterface mapper) throws SQLException {
         String sql = MessageFormat.format("SELECT * FROM {0};", tableName.getTableName());
         Statement stmt = conn.createStatement();
         ResultSet rs = stmt.executeQuery(sql);
+        ArrayList<?> entities = mapper.fromResultSet(rs);
         stmt.close();
+        return entities;
     }
 
     public int insert(TableEnum tableName, List<String> fieldList, List<Object> valueList) throws SQLException {
+        if (fieldList.size() != valueList.size()) throw new SQLException("FIELD OR VALUE FORMAT IS INCORRECT");
         String fields = fieldList
                 .stream()
                 .collect(Collectors.joining(", ", "(", ")"));
