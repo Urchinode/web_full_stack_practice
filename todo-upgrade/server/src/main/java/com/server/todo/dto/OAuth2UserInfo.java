6e040e03053865880a76a6ff856d8ed326afb1af
Sin Han <vrdhan212@gmail.com>
2024-07-18 16:30:55 +0900

    [ug-todo|server] update: set OAuthUserInfo attrs

--- todo-upgrade/server/src/main/java/com/server/todo/dto/OAuth2UserInfo.java
+++ todo-upgrade/server/src/main/java/com/server/todo/dto/OAuth2UserInfo.java
@@ -6,7 +6,7 @@ import jakarta.security.auth.message.AuthException;
 import lombok.Builder;
 import org.springframework.security.crypto.keygen.KeyGenerators;
 
-import javax.crypto.KeyGenerator;
+import java.text.MessageFormat;
 import java.util.Map;
 
 @Builder
@@ -15,21 +15,31 @@ public record OAuth2UserInfo(
         String email,
         String gender
 ) {
+    @Override
+    public String toString() {
+        return MessageFormat.format("NICKNAME: {0}, EMAIL: {1}, GENDER: {2}", nickname, email, gender);
+    }
+
     public static OAuth2UserInfo of(String registrationId, Map<String, Object> attrs) throws AuthException {
         return switch (registrationId) {
-            case "kakao" -> ofKakao(attrs);
+            case "id" -> ofKakao(attrs);
             default -> throw new AuthException("FAIL");
         };
     }
 
     private static OAuth2UserInfo ofKakao(Map<String, Object> attrs) {
         Map<String, Object> account = (Map<String, Object>) attrs.get("kakao_account");
-        Map<String, Object> profile = (Map<String, Object>) attrs.get("profile");
         System.out.println(account);
-        System.out.println(profile);
+        String nickname = (String) ((Map<String, Object>) account.get("profile")).get("nickname");
+        String email = (String) account.get("email");
+        String gender = (String) account.get("gender");
 
-
-        return OAuth2UserInfo.builder().build();
+        return OAuth2UserInfo
+                .builder()
+                .nickname(nickname)
+                .email(email)
+                .gender(gender)
+                .build();
     }
 
     public UserEntity toUserEntity() {
