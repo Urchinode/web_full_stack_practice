70ac7b753e4f8d5fa7ed0986f7bf9b2f65d5a256
Sin Han <vrdhan212@gmail.com>
2024-07-20 18:15:53 +0900

    [ug-todo|server] feat: global logger

---
+++ todo-upgrade/server/src/main/java/com/server/todo/utils/Logger.java
@@ -0,0 +1,12 @@
+package com.server.todo.utils;
+
+import org.slf4j.LoggerFactory;
+
+public class Logger {
+    private Logger() {
+    }
+
+    public static org.slf4j.Logger getLogger(Class<?> clazz) {
+        return LoggerFactory.getLogger(clazz.getName());
+    }
+}
